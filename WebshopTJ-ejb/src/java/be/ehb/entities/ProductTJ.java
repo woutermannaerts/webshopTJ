0b838fa91713d0f4a4d2b343518a9dcaae3d7a7a
mobapp03 <mobapp03@10.3.30.4>
2017-01-30 15:28:40 +0100

    Entities en persistence aangemaakt en database verbonden

---
+++ WebshopTJ-ejb/src/java/be/ehb/entities/ProductTJ.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ehb.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author mobapp03
+ */
+@Entity
+@Table(name = "productTJ")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ProductTJ.findAll", query = "SELECT p FROM ProductTJ p"),
+    @NamedQuery(name = "ProductTJ.findById", query = "SELECT p FROM ProductTJ p WHERE p.id = :id"),
+    @NamedQuery(name = "ProductTJ.findByPrijs", query = "SELECT p FROM ProductTJ p WHERE p.prijs = :prijs")})
+public class ProductTJ implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "naam")
+    private String naam;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "categorie")
+    private String categorie;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "prijs")
+    private double prijs;
+
+    public ProductTJ() {
+    }
+
+    public ProductTJ(Integer id) {
+        this.id = id;
+    }
+
+    public ProductTJ(Integer id, String naam, String categorie, double prijs) {
+        this.id = id;
+        this.naam = naam;
+        this.categorie = categorie;
+        this.prijs = prijs;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(double prijs) {
+        this.prijs = prijs;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ProductTJ)) {
+            return false;
+        }
+        ProductTJ other = (ProductTJ) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "be.ehb.entities.ProductTJ[ id=" + id + " ]";
+    }
+    
+}
---
+++ WebshopTJ-ejb/src/java/be/ehb/entities/Users.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ehb.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author mobapp03
+ */
+@Entity
+@Table(name = "Users")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
+    @NamedQuery(name = "Users.findById", query = "SELECT u FROM Users u WHERE u.id = :id"),
+    @NamedQuery(name = "Users.findByTelefoonnummer", query = "SELECT u FROM Users u WHERE u.telefoonnummer = :telefoonnummer"),
+    @NamedQuery(name = "Users.findByCreditcard", query = "SELECT u FROM Users u WHERE u.creditcard = :creditcard")})
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "naam")
+    private String naam;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "adres")
+    private String adres;
+    @Column(name = "telefoonnummer")
+    private Integer telefoonnummer;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "creditcard")
+    private int creditcard;
+
+    public Users() {
+    }
+
+    public Users(Integer id) {
+        this.id = id;
+    }
+
+    public Users(Integer id, String naam, String adres, String email, int creditcard) {
+        this.id = id;
+        this.naam = naam;
+        this.adres = adres;
+        this.email = email;
+        this.creditcard = creditcard;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public Integer getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(Integer telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCreditcard() {
+        return creditcard;
+    }
+
+    public void setCreditcard(int creditcard) {
+        this.creditcard = creditcard;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "be.ehb.entities.Users[ id=" + id + " ]";
+    }
+    
+}
